9493d313b61d83084a8d31ea289a6ff4ffc8ee67
Angelina Andronova <angelina.andronova@xapo.com>
2021-11-04 12:26:51 +0100

    Initial Commit

---
+++ SafeCryptoTrader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SafeCryptoTrader
+//
+//  Created by Angelina Andronova on 04.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
